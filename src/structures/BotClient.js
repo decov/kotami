f15d1efd77e5ec4fddba5fb53dfdaab4d099767c
furstamp <gyket.pentest.133971@protonmail.com>
2023-02-11 17:18:57 -0300

    estrutura botclient + consts

---
+++ src/structures/BotClient.js
@@ -0,0 +1,12 @@
+// consts
+const { 
+    Client,
+    Collection,
+    GatewayIntentBits,
+    Partials,
+    WebhookClient,
+    ApplicationCommandType
+} = require('discord.js');
+
+const path = require('path');
+const { table } = require('table');
