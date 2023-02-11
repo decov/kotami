288cba5ebb7951ef492f1594a872be58e62dc4fd
furstamp <gyket.pentest.133971@protonmail.com>
2023-02-11 17:30:07 -0300

    citação de funções do fs no 'utils.js'

--- src/helpers/Utils.js
+++ src/helpers/Utils.js
@@ -1,3 +1,4 @@
 // consts
-const { COLORS } = require("@src/data.json");
+const { COLORS } = require('@src/data.json');
+const { readdirSync, lstatSync } = require('fs');
 const { join, extname } = require('path');
