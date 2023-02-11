8f2998b3ef726aae5ee9863db0aebb4f85f58bfc
furstamp <gyket.pentest.133971@protonmail.com>
2023-02-11 18:56:50 -0300

    criação de arquivo index para structures

---
+++ src/structures/index.js
@@ -0,0 +1,12 @@
+// consts
+const BotClient = require('./BotClient');
+const Command = require('./Command');
+/* const CommandCategory = require('./CommandCategory'); */
+const BaseContext = require('./BaseContext');
+
+module.exports = {
+    BaseContext,
+    BotClient,
+    Command,
+    /* CommandCategory */
+};
