4ab783fab80fb6cd19d3245dde3b093363d01601
furstamp <gyket.pentest.133971@protonmail.com>
2023-02-11 22:07:52 -0300

    canais e seus respectivos tipos

---
+++ src/helpers/channelTypes.js
@@ -0,0 +1,41 @@
+const { ChannelType } = require('discord.js');
+
+/**
+ * @param {number} type
+ */
+module.exports = (type) => {
+    switch (type) {
+        case ChannelType.GuildText:
+            return "guild text";
+        
+        case ChannelType.GuildVoice:
+            return "guild voice";
+
+        case ChannelType.GuildCategory:
+            return "guild category";
+
+        case ChannelType.GuildAnnouncement:
+            return "guild announcement";
+
+        case ChannelType.AnnouncementThread:
+            return "guild announcement thread";
+
+        case ChannelType.PublicThread:
+            return "guild public thread";
+
+        case ChannelType.PrivateThread:
+            return "guild private thread";
+
+        case ChannelType.GuildStageVoice:
+            return "guild stage voice";
+
+        case ChannelType.GuildDirectory:
+            return "guild directory";
+
+        case ChannelType.GuildForum:
+            return "guild forum";
+
+        default:
+            return "unknown";
+    }
+};
