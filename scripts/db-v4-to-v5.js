d3ad204f9f5b1144066dcf82f1e1b5369a07e42b
furstamp <gyket.pentest.133971@protonmail.com>
2023-02-16 00:31:57 -0300

    segunda etapa da database v4tov5 do mongoose

--- scripts/db-v4-to-v5.js
+++ scripts/db-v4-to-v5.js
@@ -65,3 +65,175 @@ const clearAndLog = (message) => {
 
     console.log(message);
 };
+
+/**
+ * migrar coleção de mod-logs da v4 para v5
+ * @param {mongoose.Collection<mongoose.Document>[]} collection
+ */
+const migrateGuilds = async (collections) => {
+    process.stdout.write('migrando coleção "guilds"...');
+
+    try {
+        const guildsC = collections.find((c) => c.collectionName === 'guilds');
+
+        const toUpdate = await guildsC
+            .find({
+                $or: [
+                    {
+                        'data.owner': { 
+                            $type: "object" 
+                        }
+                    },
+                    {
+                        "automod.strikes": 5 
+                    },
+                    {
+                        "automod.action": "MUTE" 
+                    },
+                    {
+                        "automod.anti_scam": { 
+                            $exists: true 
+                        } 
+                    },
+                    {
+                        "max_warn.strikes": 5 
+                    },
+                    {
+                        ranking: { 
+                            $exists: true 
+                        } 
+                    }
+                ]
+            })
+            .toArray();
+    
+        if (toUpdate.length > 0) {
+            for (const doc of toUpdate) {
+                if (typeof doc.data.owner === 'object')
+                    doc.data.owner = doc.data.owner.id;
+
+                if (typeof doc.automod === 'object') {
+                    if (doc.automod.strikes === 5)
+                        doc.automod.strikes = 10;
+
+                    if (doc.automod.action === 'MUTE')
+                        doc.automod.action = 'TIMEOUT';
+
+                    doc.automod.anti_spam = doc.automod.anti_scam || false;
+                }
+
+                if (typeof doc.max_warn === 'object') {
+                    if (doc.max_warn.action === 'MUTE')
+                        doc.automod.action = 'TIMEOUT';
+
+                    if (doc.max_warn.action === 'BAN')
+                        doc.automod.action = 'KICK';
+                }
+
+                if (typeof doc.stats !== 'object')
+                    doc.stats = {};
+
+                if (doc.ranking?.enabled)
+                    doc.stats.enabled = true;
+
+                await guildsC.updateOne(
+                    {
+                        _id: doc._id 
+                
+                    },
+                    { 
+                        $set: doc 
+                    }
+                );
+        
+                process.stdout.clearLine();
+                process.stdout.cursorTo(0);
+
+                process.stdout.write(
+                    `migrando coleção "guilds" collection | completo - ${Math.round(
+                        (toUpdate.indexOf(doc) / toUpdate.length) * 100
+                    )}%...`
+                );
+            }
+    
+            await guildsC.updateMany(
+                {},
+                {
+                    $unset: {
+                        'automod.anti_scam': '',
+                        'automod.max_mentions': '',
+                        'automod.max_role_mentions': '',
+
+                        ranking: ''
+                    }
+                }
+            );
+    
+            clearAndLog(`migrando coleção 'guilds' | atualizado: ${toUpdate.length}...`);
+        } else {
+            clearAndLog('migrando coleção "guilds" | sem atualizações necessárias...');
+        }
+    } catch (ex) {
+        clearAndLog('migrando coleção "guilds" | ocorreu um erro...');
+
+        console.log(ex);
+    }
+};
+
+/**
+ * migrar coleção mod-logs da v4 para a v5
+ * @param {mongoose.Collection<mongoose.Document>[]} collection
+ */
+const migrateModLogs = async (collections) => {
+    process.stdout.write('migrando coleção "mod-logs"...');
+
+    try {
+        const modLogs = collections.find((c) => c.collectionName === 'mod-logs');
+
+        const stats = await modLogs.updateMany(
+            {}, { 
+                $unset: { 
+                    expires: '' 
+                } 
+            }
+        );
+
+        await modLogs.updateMany(
+            { 
+                type: 'MUTE' 
+            },
+            { 
+                $set: { 
+                    type: 'TIMEOUT' 
+                } 
+            }
+        );
+
+        await modLogs.updateMany(
+            { 
+                type: 'UNMUTE' 
+            }, 
+            { 
+                $set: { 
+                    type: 'UNTIMEOUT' 
+                } 
+            }
+        );
+
+        console.log(`| ${stats.modifiedCount > 0 ? `atualizado: ${stats.modifiedCount}` : 'sem atualizações necessárias...'}`);
+    } catch (ex) {
+        clearAndLog('migrando coleção "mod-logs" | ocorreu um erro...');
+        
+        console.log(ex);
+    }
+};
+
+/**
+ * migrar coleção de translate-logs da v4 para a v5
+ * @param {mongoose.Collection<mongoose.Document>[]} collections
+ */
+const migrateTranslateLogs = async (collections) => {
+    process.stdout.write('migrando coleção "translate-logs"...');
+
+    console.log('| sem atualizações necessárias...');
+};
