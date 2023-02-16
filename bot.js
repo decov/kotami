1f0aab42fe27d66d52198acc6ff335ce1ad4c6ee
furstamp <gyket.pentest.133971@protonmail.com>
2023-02-16 00:58:14 -0300

    adição para extenders do helpers

--- bot.js
+++ bot.js
@@ -1,2 +1,8 @@
 // requires
 require('dotenv').config();
+require('module-alias/register');
+
+// extenders de registro
+require('@helpers/extenders/Message');
+require('@helpers/extenders/Guild');
+require('@helpers/extenders/GuildChannel');
