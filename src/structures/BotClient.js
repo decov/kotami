06f25fbdcccd32dd1df918ee22469aca20436594
furstamp <gyket.pentest.133971@protonmail.com>
2023-02-26 13:33:30 -0300

    adição de alguns consts no arquivo 'BotClient'

--- src/structures/BotClient.js
+++ src/structures/BotClient.js
@@ -10,3 +10,13 @@ const {
 
 const path = require('path');
 const { table } = require('table');
+const { DiscordTogether } = require('discord-together');
+
+const Logger = require('../helpers/Logger');
+const CommandCategory = require('./CommandCategory');
+const lavaclient = require('../handlers/lavaclient');
+const giveawaysHandler = require('../handlers/giveaway');
+
+const { recursiveReadDirSync } = require('../helpers/Utils');
+const { validateCommand, validateContext } = require('../helpers/Validator');
+const { schemas } = require('@src/database/mongoose');
