e49a3df298cf74626cc13d01ffb00e06d76cd62b
furstamp <gyket.pentest.133971@protonmail.com>
2023-02-26 14:54:19 -0300

    finalização do arquivo main do bot em si

--- bot.js
+++ bot.js
@@ -17,3 +17,41 @@ validateConfiguration();
 
 // inicializando o client
 const client = new BotClient();
+
+client.loadCommands('src/commands');
+client.loadContexts('src/contexts');
+client.loadEvents('src/events');
+
+// encontrar rejeições de promessa não tratadas
+process.on(
+    'unhandledRejection', (err) => client.logger.error(
+        `excessão não tratada...`, err
+    )
+);
+
+(async () => {
+    // checar por updates
+    await checkForUpdates();
+
+    // inicializar o dashboard
+    if (client.config.DASHBOARD.enabled) {
+        client.logger.log('executando dashboard...');
+
+        try {
+            const { launch } = require('@root/dashboard/app');
+
+            // dashboard inicializando o banco de dados
+            await launch(client);
+        } catch (ex) {
+            client.logger.error('ocorreu um erro ao tentar executar o dashboard...', ex);
+        }
+    } else {
+        // inicializando o banco de dados
+        await initializeMongoose();
+    }
+
+    // inicializa o client
+    await client.login(
+        process.env.BOT_TOKEN
+    );
+})();
