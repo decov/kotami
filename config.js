b3bdf223a3d0c67519d628c407a6331cc0921569
furstamp <gyket.pentest.133971@protonmail.com>
2023-02-26 15:01:58 -0300

    primeira etapa do arquivo de config criada

---
+++ config.js
@@ -0,0 +1,37 @@
+module.exports = {
+    OWNER_IDS: [
+        "1070476206660665376"
+    ],
+
+    SUPPORT_SERVER: "xxxxxxxxxxx",
+
+    PREFIX_COMMANDS: {
+        ENABLED: true,
+        DEFAULT_PREFIX: "--"
+    },
+
+    INTERACTIONS: {
+        SLASH: true,
+        CONTEXT: true,
+        GLOBAL: true,
+        TEST_GUILD_ID: "xxxxxxxxxxx"
+    },
+
+    EMBED_COLORS: {
+        BOT_EMBED: "#36393F",
+        TRANSPARENT: "#36393F",
+        SUCCESS: "#00A56A",
+        ERROR: "#D61A3C",
+        WARNING: "#F7E919"
+    },
+
+    CACHE_SIZE: {
+        GUILDS: 100,
+        USERS: 10000,
+        MEMBERS: 10000
+    },
+
+    MESSAGES: {
+        API_ERROR: "erro inesperado de backend. tente novamente mais tarde..."
+    }
+}
