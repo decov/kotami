44737bbb0ace9d89e20e88e79c74e46305ec10f3
furstamp <gyket.pentest.133971@protonmail.com>
2023-02-26 13:25:42 -0300

    adição de alguns consts + inicializador de client

--- bot.js
+++ bot.js
@@ -6,3 +6,14 @@ require('module-alias/register');
 require('@helpers/extenders/Message');
 require('@helpers/extenders/Guild');
 require('@helpers/extenders/GuildChannel');
+
+const { checkForUpdates } = require('@helpers/BotUtils');
+const { initializeMongoose } = require('@src/database/mongoose');
+const { BotClient } = require('@src/structures');
+const { validateConfiguration } = require('@helpers/Validator');
+
+// validar configurações feitas no bot
+validateConfiguration();
+
+// inicializando o client
+const client = new BotClient();
