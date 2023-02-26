53baff8cea8b1c2f0e73b674226a13a21f8d0adc
furstamp <gyket.pentest.133971@protonmail.com>
2023-02-26 12:31:15 -0300

    arquivo principal para rotas do dashboard

---
+++ dashboard/routes/index.js
@@ -0,0 +1,17 @@
+// consts
+const express = require('express'),
+    CheckAuth = require('../auth/CheckAuth'),
+    router = express.Router();
+
+router.get('/', CheckAuth, async (req, res) => {
+    res.redirect('/selector');
+});
+
+router.get('/selector', CheckAuth, async (req, res) => {
+    res.render('selector', {
+        user: req.userInfos,
+        currentURL: `${req.client.config.DASHBOARD.baseURL}/${req.originalUrl}`
+    });
+});
+
+module.exports = router;
